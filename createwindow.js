f76a321175f66c8c362f547f958f66ffca18eb27
Jakub Starostecki <jakub.starostecki27@gmail.com>
2022-02-11 10:57:51 +0100

    release

--- createwindow.js
+++ createwindow.js
@@ -1,20 +1,20 @@
 const {app, BrowserWindow} = require('electron');
-
 let win;
 function createWindow (){
     win = new BrowserWindow({
         height: 1080,
         width:1920,
-        backgroundColor:'#ffffff'
+        autoHideMenuBar: true,
+        webPreferences: {
+            devTools: false
+        }
     })
     win.loadURL(`file://${__dirname}/index.html`)
     win.on('closed',function(){
         win=null;
     })
 }
-
 app.on('ready',createWindow)
-
 app.on('windows-all-closed',()=>{
     if(process.platform!=='darwin'){
         app.quit();
